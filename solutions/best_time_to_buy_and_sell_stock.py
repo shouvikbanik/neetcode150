7408d6718f84b51a85ff6b19b4988084e085a7c4
shouvikbanik <shouvikbanik96@gmail.com>
2023-09-30 15:16:04 +0530

    ->best_time_to_buy_and_sell_stock solution

---
+++ solutions/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,15 @@
+def max_profit(prices):
+    """
+    :type prices: List[int]
+    :rtype: int
+    """
+    buy = 0
+    sell = 1
+    profit_max = 0
+    while (sell < len(prices)):
+        if prices[buy] < prices[sell]:
+            profit_max = max(profit_max, prices[sell] - prices[buy])
+        else:
+            buy=sell
+        sell+=1
+    return profit_max
---
+++ test_solutions/test_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+from best_time_to_buy_and_sell_stock import max_profit
+
+
+class Test(TestCase):
+    def test_max_profit(self):
+        self.assertEqual(max_profit([7,1,5,3,6,4]),5)
