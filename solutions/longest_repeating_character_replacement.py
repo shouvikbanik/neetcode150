fbceb5ec5ab97472e74a60b3a95b7509dd474bdd
shouvikbanik <shouvikbanik96@gmail.com>
2023-11-18 17:53:28 +0530

    ->longest_repeating_character_replacement solution

--- solutions/longest_repeating_character_replacement.py
+++ solutions/longest_repeating_character_replacement.py
@@ -1,7 +1,24 @@
 def character_replacement(s, k):
     """
-    :type s: str
-    :type k: int
-    :rtype: int
+    :type s: str AABABBA
+    :type k: int 1
+    :rtype: int 4
     """
-    pass
+    longest_length = 0
+    char_dict = {s[0]: 1}
+    left = 0
+    right = 1
+    length = len(s)
+    while right != length:
+        if s[right] not in char_dict.keys():
+            char_dict[s[right]] = 1
+        else:
+            char_dict[s[right]] += 1
+        highest_char_freq = max(char_dict.values())
+        if (right - left + 1) - highest_char_freq <= k:
+            longest_length = max(longest_length, right - left + 1)
+        else:
+            char_dict[s[left]] -= 1
+            left += 1
+        right += 1
+    return longest_length
--- solutions/tests/test_longest_repeating_character_replacement.py
+++ solutions/tests/test_longest_repeating_character_replacement.py
@@ -6,3 +6,6 @@ from longest_repeating_character_replacement import character_replacement
 class Test(TestCase):
     def test_character_replacement(self):
         self.assertEqual(4, character_replacement("AABABBA", 1))
+
+    def test_character_replacement2(self):
+        self.assertEqual(4, character_replacement("ABAB", 2))
